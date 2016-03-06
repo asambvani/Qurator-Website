00f78bd4d3bff34028a44bf7bf750f94e8a55005
asambvani <alexsambvani@gmail.com>
2016-03-06 12:20:08 -0500

    added dynamic menu

--- js/navigation_bar.js
+++ js/navigation_bar.js
@@ -1,11 +1,11 @@
 $(window).load(function(){
     $(function(){
-        window.alert("THIS IS A TEST"); 
+     
         var nav = $(".floating_nav_background"); 
         var navHomeY = nav.offset().top; 
         var isVisible = false; 
         var $w = $(window); 
-        window.alert("test");
+        
         $w.scroll(function(){
             var scrollTop = $w.scrollTop(); 
             console.log(scrollTop); 
@@ -13,7 +13,7 @@ $(window).load(function(){
             if (shouldBeVisible && !isVisible){
 
                 nav.css({
-                    visibility: 'visible';
+                    visibility: 'visible'
                     
                 }); 
 
@@ -22,7 +22,7 @@ $(window).load(function(){
             else if (!shouldBeVisible && isVisible) {
                
                 nav.css({
-                    visibility: 'hidden'; 
+                    visibility: 'hidden' 
                 });
                 isVisible = false; 
 
